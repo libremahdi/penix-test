bf97e6f91348ae1239c30d607b1964a4288ef332
Mahdi Hosseini Asaad <libre.hosseini@Gmail.com>
2026-01-03 11:55:21 +0330

    Update: ptest_init.c ptest_run.c
    The issue with the ptst_comment text position has been resolved.

--- src/ptest_init.c
+++ src/ptest_init.c
@@ -1,13 +1,15 @@
-#include "ptest.h"
-#include "struct.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "ptest.h"
+#include "struct.h"
 
 ptst_result *ptst_init(char *topic) {
     ptst_result *result=malloc(sizeof(ptst_result));
     result->topic=malloc(strlen(topic)*sizeof(char)+1);
     strcpy(result->topic, topic);
-
+    printf ("\n========= %s =========\n", topic);
     result->scenario=NULL;
     result->next_scenario=0;
     return result;
