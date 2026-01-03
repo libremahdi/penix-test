1e35e70b7645ee504765087066246626b221ab5f
Mahdi Hosseini Asaad <libre.hosseini@Gmail.com>
2026-01-03 10:48:54 +0330

    Update: ptest_run.c | style[stdout]: Change and improve the stdout interface and better messages.

--- src/ptest_run.c
+++ src/ptest_run.c
@@ -12,21 +12,19 @@
 
 void ptst_run(ptst_result *(*ptt_fnc)()) {
     ptst_result *result = ptt_fnc();
-    char fail_flag = 0;
-
+    printf ("\n========= %s =========\n",result->topic);
     for (register unsigned int in_1=0 ; in_1<result->next_scenario ; ++in_1) {
         if (result->scenario[in_1]->failds != 0) {
             printf (RED"[ FAIL ]: "RESET);
             printf (YELLOW"%s \n"RESET, result->topic);
             printf ("Faild: %d | %s\n\n", result->scenario[in_1]->failds, result->scenario[in_1]->details);
-            fail_flag=1;
+        }
+        else {
+            printf (GREEN"[ PASS ]: "RESET);
+            printf ("%s\n", result->scenario[in_1]->details);
         }
         free(result->scenario[in_1]->details);
         free(result->scenario[in_1]);
-    }
-    if (fail_flag==0) {
-        printf (GREEN"[ PASS ]: "RESET);
-        printf ("%s\n", result->topic);
     }
 
 
